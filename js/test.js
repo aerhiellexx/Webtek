3def14767daaa8914f4eedbcde06905b82e623d0
aerhiellexx <2153674@slu.edu.ph>
2018-04-09 15:04:49 +0800

    Delete test.js

--- js/test.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('[class^="content"]').on('click', function(e){
-        e.preventDefault();
-        var numb = this.className.replace('content', '');
-        $('[id^="topic"]').hide();
-        $('#topic' + numb).show();
-    });
-
-});
